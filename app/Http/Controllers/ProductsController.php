857df3c643a8056590f7fb75b0bb47eb65c5ee01
Lucas Ribeiro <lucas.alves@braip.com>
2023-11-30 17:55:56 -0300

    Criando Controllers para API

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,14 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductsResource;
 use App\Models\Products;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use App\Http\Resources\ProductsResource;
-use App\Http\Requests\ProductUpdateRequest;
-
+use Illuminate\Routing\Controller;
 
 class ProductsController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Products::paginate(5);
+
+        return ProductsResource::collection($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
